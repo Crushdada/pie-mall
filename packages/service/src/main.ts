8a79ae6df08e8cfc34abe53961e576d589baf048
David <2265997266@qq.com>
2022-01-18 21:16:47 +0800

    feat(application): init mono-repo

---
+++ packages/service/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+async function bootstrap() {
+  // main.ts 中配置
+  // api文档插件
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // DocumentBuilder是一个辅助类&#xff0c;有助于结构的基本文件SwaggerModule。它包含几种方法&#xff0c;可用于设置诸如标题&#xff0c;描述&#xff0c;版本等属性。
+  const options = new DocumentBuilder()
+    .setTitle('nest入门接口标题')
+    .setDescription('使用nest书写的常用性接口') // 文档介绍
+    .setVersion('1.0.0') // 文档版本
+    .addTag('用户,安全') // 每个tag标签都可以对应着几个&#64;ApiUseTags('用户,安全') 然后被ApiUseTags注释&#xff0c;字符串一致的都会变成同一个标签下的
+    // .setBasePath('http://localhost:5000')
+    .build();
+  // 为了创建完整的文档;具有定义的HTTP路由&#xff09;&#xff0c;我们使用类的createDocument()方法SwaggerModule。此方法带有两个参数&#xff0c;分别是应用程序实例和基本Swagger选项。
+  const document = SwaggerModule.createDocument(app, options);
+  // 最后一步是setup()。它依次接受&#xff08;1&#xff09;装入Swagger的路径&#xff0c;&#xff08;2&#xff09;应用程序实例, &#xff08;3&#xff09;描述Nest应用程序的文档。
+  SwaggerModule.setup('/docs', app, document);
+  await app.listen(3000);
+}
+bootstrap();
