9ae071b37c7fe7540a52b905297c92a60cc595de
David <2265997266@qq.com>
2022-01-25 00:53:56 +0800

    feat(admin & app): 项目工程化

--- packages/admin/src/main.ts
+++ packages/admin/src/main.ts
@@ -2,8 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueLazyload from "vue-lazyload";
 
 Vue.config.productionTip = false;
+Vue.config.devtools = true;
+
+Vue.use(VueLazyload);
 
 new Vue({
   router,
--- packages/admin/src/store/index.ts
+++ packages/admin/src/store/index.ts
@@ -1,11 +1,19 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { Store } from "vuex";
+// import { VuexModuleName } from "&/types/vuex/enums/module-name.enum";
+// import { VuexRootState } from "@/types/vuex/root-state.interface";
+import authModule from "./auth.module";
 
 Vue.use(Vuex);
+const store = new Vuex.Store<VuexRootState>({});
+export default store;
+store.registerModule(VuexModuleName.WORKSPACE, authModule);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+// vue store type pollyfill
+// this $store type problem is only subject to change when using vue3
+/// <reference path="https://github.com/vuejs/vuex/releases/tag/v4.0.0-beta.1" />
+Object.defineProperty(Vue.prototype, "$stock", {
+  get(): Store<VuexRootState> {
+    return this.$store;
+  },
 });
